939d6265f45f603addc32170e47c5f0795898e7f
Nour490-code <nourdin.dna@gmail.com>
2024-09-14 23:42:05 +0300

    feat: add mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import route from "./routes/route.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use("/api", route);
 
 app.listen(port, () => {
+    connectDB();
     console.log(`Listening on port ${port}`);
 })
 
