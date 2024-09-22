988e66519572ec4de0ba1d3376ddba6f36f18803
Nour490-code <nourdin.dna@gmail.com>
2024-09-22 10:26:47 +0300

    refactor(config): add seperate env file for env. variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose"
-import dotenv from "dotenv"
-
-dotenv.config()
+import { MONGO_URI } from "../config/env.js"
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(MONGO_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@ import express, { json } from "express";
 import cors from "cors";
 import route from "./routes/route.js";
 import { connectDB } from "./config/db.js";
+import { PORT } from "./config/env.js";
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(json());
 app.use(cors());
 app.use("/api", route);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${PORT}`);
 })
 
