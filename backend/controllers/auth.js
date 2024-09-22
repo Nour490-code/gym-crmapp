5f19247f7a4172916d84ecd177c44d6e4b0a789a
Nour490-code <nourdin.dna@gmail.com>
2024-09-22 11:12:45 +0300

    feat(registration): add register route, add validation middleware, add authentication controller.

---
+++ backend/controllers/auth.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+
+
+export const register = async (req, res) => {
+
+    const { name, email, password } = req.body;
+    try {
+        const newUser = new User({
+            name,
+            email,
+            password,
+        });
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(400).json({
+                status: "failed",
+                data: [],
+                message: "It seems you already have an account, please log in instead.",
+            });
+        const savedUser = await newUser.save(); 
+        const { role, ...user_data } = savedUser._doc;
+        res.status(200).json({
+            status: "success",
+            data: [user_data],
+            message:
+                "Thank you for registering with us. Your account has been successfully created.",
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Internal Server Error",
+        });
+    }
+    res.end();
+}
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,8 +1,32 @@
 import express from "express";
-import { dashboard } from "../controllers/gym.controllers.js";
+import { dashboard, login } from "../controllers/gym.controllers.js";
+import { register } from "../controllers/auth.js";
+import Validate from "../middleware/validate.js";
+import { check } from "express-validator";
 
 const router = express.Router();
 
 router.get("/", dashboard)
+router.get('/login', login)
+
+router.post(
+    "/register",
+    check("email")
+        .isEmail()
+        .withMessage("Enter a valid email address")
+        .normalizeEmail(),
+    check("name")
+        .not()
+        .isEmpty()
+        .withMessage("You first name is required")
+        .trim()
+        .escape(),
+    check("password")
+        .notEmpty()
+        .isLength({ min: 6 })
+        .withMessage("Must be at least 8 chars long"),
+    Validate,
+    register
+);
 
 export default router
